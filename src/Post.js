7ec8b7590fc6f4427c2ba45cef03b37d7374fb42
ktx203 <78192053+ktx203@users.noreply.github.com>
2021-12-08 11:33:37 -0500

    centered the posts

--- src/Post.js
+++ src/Post.js
@@ -4,15 +4,14 @@ import './Post.css'
 
 function Post({username,caption,imageUrl}) {
     return (
+        <div className="postCenter">
         <div className="post">
-        <div className="post_header">
-            <h3>{username}</h3>
-        </div>
-
-
+            <div className="post_header">
+                <h3>{username}</h3>
+            </div>
             <img className="post_image" src={imageUrl} alt=""></img>
             <h4 className="post_text"><strong>{username}</strong>{caption}</h4>
-
+        </div>
         </div>
     )
 }
